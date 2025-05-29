656d1c50548b31abaa5fffc79a8de3a026c2d989
Jiehao <jiehaoji530@gmail.com>
2025-05-29 19:19:59 +0200

    login created

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,10 +7,15 @@ from services.client import client
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from bson import ObjectId
+from jose import jwt, JWTError
 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = timedelta(hours=1)
+SECRET = "e3f1a8b7c9d6e4f2a1b0c3d5e7f8a9b6c4d2e0f1a3b5c7d9e6f4a2b0c1d3e5f7"
 crypt = CryptContext(schemes=["bcrypt"])
 
 router = APIRouter()
+outh2 = OAuth2PasswordBearer(tokenUrl="/login")
 
 def search_user(field: str, value):
     try:
@@ -19,6 +24,14 @@ def search_user(field: str, value):
         return UserDataBase(**search_dict)
     except:
         return None
+
+def get_token(user: str):
+    data_token = {
+        "sub": user,
+        "exp": datetime.utcnow + ACCESS_TOKEN_DURATION
+    }
+    token = jwt.encode(data_token, SECRET, algorithm=ALGORITHM)
+    return token
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup_user(data: User):
@@ -36,3 +49,15 @@ async def signup_user(data: User):
     id = client.users.insert_one(data_dict).inserted_id
     new_user = search_user("_id", ObjectId(id))
     return user_scheme_final(dict(new_user))
+
+@router.post("/login")
+async def login_user(form: OAuth2PasswordRequestForm = Depends()):
+    user = search_user("username", form.username)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This user doesn't exist")
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+    
+    token = get_token(user.username)
+    return {"Token": token, "Token_type": "Bearer"}
+    
