4d8fb47dbbb7d2096952e7fb7b3130635bb2e130
Jiehao <jiehaoji530@gmail.com>
2025-06-02 17:09:28 +0200

    test_signup_user created

--- test/test_users.py
+++ test/test_users.py
@@ -6,3 +6,15 @@ from app.routers.users import router
 from fastapi.testclient import TestClient
 
 client = TestClient(router)
+
+def test_signup_user():
+    data = {
+        "username": "UserTester",
+        "password": "Testing123",
+        "email": "usertester@gmail.com",
+        "department": "Testing",
+        "rol": "admin"
+        }
+    response = client.post("/signup", json=data)
+    assert response.status_code == 201
+    assert response.json()["username"] == "UserTester"
